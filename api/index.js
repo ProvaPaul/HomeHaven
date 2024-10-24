1ab4fc8adc508cf83948ed5704129c27ab576d8f
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-24 11:20:38 +0600

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+    }   
+).catch((error) => {
+    console.log('Error:', error.message);
+    }
+);
+const port = process.env.PORT || 4000;
+
+
 
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
     }
 );
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is running on http://localhost:3000');
     }
 );
