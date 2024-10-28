609799777ffd40f151b03bef4cd68ba2f7c87e2d
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-28 08:21:08 +0600

    completed signIn page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,11 +51,11 @@ export default function SignUp() {
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" onClick={handleChange}/>
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" onChange={handleChange}/>
 
-        <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onClick={handleChange}/>
+        <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onChange={handleChange}/>
 
-        <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onClick={handleChange}/>
+        <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={handleChange}/>
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:oacity-80">
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
