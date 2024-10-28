8ee03b411287da49edc84a7608e939e07870ef6f
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-28 07:59:22 +0600

    create SignIn api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import {errorHandler} from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async(req, res,next) => {
     const {username, email, password} = req.body;
     const hashPassword = bcryptjs.hashSync(password, 10);
@@ -12,5 +14,31 @@ export const signup = async(req, res,next) => {
     catch(error){
         next(error);
     }
-   
+}
+export const signin = async(req, res,next) => {
+    const {email, password} = req.body;
+    try{
+      const validUser=await User.findOne({
+        email
+      })
+      if(!validUser){
+        return next(errorHandler(404,'User not found!'));
+      }
+      const validPassword=bcryptjs.compareSync(password, validUser.password);
+      if(!validPassword){
+        return next(errorHandler(400,'Invalid password!'));
+      }
+      // after signing up successfully we have to save the token as cookies in the browser in hash format
+      const token=jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+      // for not returning password in response
+      const { password:pass,...rest }=validUser._doc;
+
+      res.cookie ('access_token',token,{httpOnly:true})
+      .status(200)
+      .json(rest);
+
+    }
+    catch(error){
+      next(error);
+    }
 }
