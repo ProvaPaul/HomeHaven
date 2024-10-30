045962015a90b1bd2e7c22815b0def8d270ee831
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-30 20:06:28 +0600

    complete profile page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
-  const {currentUser} = useSelector(state => state.user)
+  const {currentUser} = useSelector((state) => state.user)
   console.log(currentUser);
 
   return (
