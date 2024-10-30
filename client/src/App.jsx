3f8d66bbcc29a2ff1b611845900bb044a1661d47
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-30 19:51:25 +0600

    made profile page at header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,17 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 export default function App() {
   return <BrowserRouter>
   <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
-      <Route path="/profile" element={<Profile/>} />
       <Route path="/signin" element={<SignIn/>} />
+      <Route element={<PrivateRoute/>} >
+      <Route path="/profile" element={<Profile/>} />
+      </Route>
       <Route path="/signup" element={<SignUp/>} />
     </Routes>
   </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,11 @@
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 export default function Header() {
+  const {currentUser} = useSelector(state => state.user)
+  console.log(currentUser);
+
   return (
     <header className="bg-slate-300 shadow-md">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -22,8 +27,12 @@ export default function Header() {
         <Link to='/about'>
         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
         </Link>
-        <Link to='/sign-in'>
-        <li className='hidden sm:inline text-slate-700 hover:underline'>SignIn</li>
+        <Link to='/profile'>
+        { currentUser ? (
+          <img className='rounded-full h-7 w-7 object-cover' src={ currentUser.avatar } alt='profile' />
+        ):(
+          <li className='hidden sm:inline text-slate-700 hover:underline'>Profile</li>
+        )}
         </Link>
         
         </ul> 
