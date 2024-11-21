8a359f9d76f4d8bb2ed7c433093961d2f9b84006
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-11-21 08:32:39 +0600

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,18 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto ">
+      <h1 className="text-3xl font-bold mb-4 text-slate-900">About HomeHaven</h1>
+      <p className="mb-4 text-slate-700">
+      Ready to find your perfect space? Explore our listings and discover the best deals, from exclusive rental offers to properties available for sale. Join the HomeHaven community today and experience real estate made easy, all in one convenient place. Let us help you find a place you can call home.
+      </p>
+      <p className="mb-4 text-slate-700">
+      At HomeHaven, we simplify the process of renting and selling properties. Our platform allows you to browse through a wide range of listings, whether you're a first-time renter or an experienced homebuyer. If you're looking to sell, our user-friendly tools help you list your property quickly and connect with serious buyers.
+      </p>
+      <p className="mb-4 text-slate-700">
+      Discover your ideal living space with HomeHaven, the perfect platform for all your real estate needs. Whether you're looking for a new home to rent, buy, or sell, we make it easy to find the right property that suits your lifestyle. From spacious apartments to cozy houses, HomeHaven is your gateway to finding the home of your dreams.
+      </p>
+      
+    </div>
   )
 }
